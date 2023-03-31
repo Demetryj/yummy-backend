e5104ab22420a018fbebef4351a30a029ec0c4ac
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-03-31 23:12:24 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://Dmytro:i05Re0fWBfDNKBQj@cluster0.lhirhjw.mongodb.net/db-yummy?retryWrites=true&w=majority";
+const PORT = 3001;
+// const BASE_URL = "http://localhost:3001";
+
+module.exports = { DB_HOST, PORT };
