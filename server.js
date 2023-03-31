97a112d3be644af59ad3ba1ef526c7c8ddc21932
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-03-31 23:12:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// const { DB_HOST, PORT = 3001 } = process.env;
+const { DB_HOST, PORT = 3001 } = require("./config");
 
-// const mongoose = require("mongoose");
-// const app = require("./app");
+mongoose.set("strictQuery", true);
 
-// const { DB_HOST, PORT = 3000 } = process.env;
-
-// mongoose.set("strictQuery", true);
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log("Database connection successful");
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
