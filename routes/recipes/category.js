3d72b9dfe5f236814fcdca60d5ed7de49a58ef5e
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-01 20:48:46 +0300

    delete

--- routes/recipes/category.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const ctrl = require('../../controllers/recipes');
-
-const categoryRouter = express.Router();
-
-categoryRouter.get('/category-list', ctrl.categories);
-
-module.exports = categoryRouter;
