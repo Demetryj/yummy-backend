01665d58c66214be925c6dcd758c7c06727f9d9e
AntoBo <anton.bondarenkooooo@gmail.com>
2023-04-01 12:57:57 +0300

    done exceprt avatar

---
+++ middlewares/index.js
@@ -0,0 +1,7 @@
+const validateBodyWrapper = require("./validateBodyWrapper");
+const auth = require("./auth");
+
+module.exports = {
+    validateBodyWrapper,
+    auth,
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const router = express.Router();
+const { authValidators } = require("../../models/user");
+const { validateBodyWrapper, auth } = require("../../middlewares/index");
+const controller = require("../../controllers/auth");
 
-const authRouter = express.Router();
+router.post("/register", validateBodyWrapper(authValidators.register), controller.register);
+router.post("/signin", validateBodyWrapper(authValidators.signin), controller.signin);
+router.put("/update", validateBodyWrapper(authValidators.update), controller.update);
+router.get("/logout", auth, controller.logout);
+router.get("/current", auth, controller.current);
 
-module.exports = authRouter;
+module.exports = router;
