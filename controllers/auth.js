08c2fe4fcc7e483f6e1dc0b95588b84aa56a2a56
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-01 20:52:39 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,89 +6,90 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (user) {
-        throw HttpError(409, "Email already in use");
-    }
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
 
-    const hashPassword = await bcrypt.hash(password, 10);
+  const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
-    res.status(201).json({
-        name: newUser.name,
-        avatarURL: newUser.avatarURL,
-    });
+  res.status(201).json({
+    name: newUser.name,
+    avatarURL: newUser.avatarURL,
+  });
 };
 
 const signin = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    const errorMsg = "Email or password invalid";
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const errorMsg = "Email or password invalid";
 
-    if (!user) {
-        throw HttpError(401, errorMsg);
-    }
+  if (!user) {
+    throw HttpError(401, errorMsg);
+  }
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if (!passwordCompare) {
-        throw HttpError(401, errorMsg);
-    }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, errorMsg);
+  }
 
-    const payload = {
-        id: user._id,
-    };
+  const payload = {
+    id: user._id,
+  };
 
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
-    await User.findByIdAndUpdate(user._id, { token });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  await User.findByIdAndUpdate(user._id, { token });
 
-    res.json({
-        token,
-    });
+  res.json({
+    token,
+  });
 };
 
 const current = async (req, res) => {
-    const { avatarURL, name } = req.user;
+  const { avatarURL, name } = req.user;
 
-    res.json({
-        avatarURL,
-        name,
-    });
+  res.json({
+    avatarURL,
+    name,
+  });
 };
 
 const logout = async (req, res) => {
-    const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { token: "" });
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-    res.json({
-        message: "Logout success",
-    });
+  res.json({
+    message: "Logout success",
+  });
 };
 
 const update = async (req, res) => {
-    const { name: newName } = req.body;
-    const newAvatarUrl = req.file?.path;
-    const { _id, name, avatarURL } = req.user;
+  const { name: newName } = req.body;
+  const newAvatarUrl = req.file?.path;
+  const { _id, name, avatarURL } = req.user;
 
-    console.log("newAvatarUrl :>> ", newAvatarUrl);
+  const updateName = !!newName ? newName : name;
+  const updateAvatarURL = !!newAvatarUrl ? newAvatarUrl : avatarURL;
 
-    const updateName = !!newName ? newName : name;
-    const updateAvatarURL = !!newAvatarUrl ? newAvatarUrl : avatarURL;
+  await User.findByIdAndUpdate(_id, {
+    name: updateName,
+    avatarURL: updateAvatarURL,
+  });
 
-    await User.findByIdAndUpdate(_id, { name: updateName, avatarURL: updateAvatarURL });
-
-    res.json({
-        name: updateName,
-        avatarURL: updateAvatarURL,
-    });
+  res.json({
+    name: updateName,
+    avatarURL: updateAvatarURL,
+  });
 };
 
 module.exports = {
-    register: ctrlWrapper(register),
-    signin: ctrlWrapper(signin),
-    current: ctrlWrapper(current),
-    logout: ctrlWrapper(logout),
-    update: ctrlWrapper(update),
+  register: ctrlWrapper(register),
+  signin: ctrlWrapper(signin),
+  current: ctrlWrapper(current),
+  logout: ctrlWrapper(logout),
+  update: ctrlWrapper(update),
 };
