c40c1d14630bc472ea29042d19bd6e0e5ff02a72
AntoBo <anton.bondarenkooooo@gmail.com>
2023-04-01 16:11:45 +0300

    conflict

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const { DB_HOST, PORT = 3001 } = process.env;
+// const { DB_HOST, PORT = 3001 } = require("./config");
 
 mongoose.set("strictQuery", true);
 
