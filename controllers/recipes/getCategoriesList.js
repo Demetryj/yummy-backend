c1bdb22901dc6aec766ec40628a20bd53508d434
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-01 20:50:32 +0300

    Create getCategoriesList.js
    rename folder "categories" -> "getCategoriesList"
    
    rename function  "categories" -> "getCategoriesList"

---
+++ controllers/recipes/getCategoriesList.js
@@ -0,0 +1,16 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const { ctrlWrapper } = require("../../helpers");
+
+const pathContacts = path.join(__dirname, "../../data/categories.json");
+
+const getCategoriesList = async (req, res) => {
+  const data = await fs.readFile(pathContacts);
+  const result = JSON.parse(data);
+  res.json({
+    category: result.sort(),
+  });
+};
+
+module.exports = { getCategoriesList: ctrlWrapper(getCategoriesList) };
