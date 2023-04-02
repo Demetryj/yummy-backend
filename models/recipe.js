dbcaea4bda12b25a22850ef06566cddd0d862a8a
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-04-02 16:19:24 +0300

    add addSchema in models recipe

--- models/recipe.js
+++ models/recipe.js
@@ -1,10 +1,8 @@
 const { Schema, model } = require("mongoose");
-
-
+const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
 
 const categoryVariants = [
-
   "Beef",
   "Breakfast",
   "Chicken",
@@ -21,79 +19,82 @@ const categoryVariants = [
   "Vegetarian",
 ];
 
-
 const recipeSchema = new Schema(
   {
-    title:
-    {
+    title: {
       type: String,
-      required: true
+      required: true,
     },
-    category:
-    {
+    category: {
       type: String,
       enum: categoryVariants,
-      required: true
+      required: true,
     },
-    area:
-    {
+    area: {
       type: String,
-      required: true
+      required: true,
     },
-    instructions:
-    {
+    instructions: {
       type: String,
-      required: true
+      required: true,
     },
-    description:
-    {
+    description: {
       type: String,
-      required: true
+      required: true,
     },
-    thumb:
-    {
+    thumb: {
       type: String,
-      required: true
+      required: true,
     },
-    preview:
-    {
+    preview: {
       type: String,
-      required: true
+      required: true,
     },
-    time:
-    {
+    time: {
       type: String,
-      required: true
+      required: true,
     },
-    popularity:
-    {
+    popularity: {
       type: Number,
-      required: true
+      required: true,
     },
-    favorites:
-    {
-      type: [Schema.Types.ObjectId]
-    },
-    likes:
-    {
-      type: Array
-    },
-    youtube:
-    {
-      type: String
-    },
-    tags:
-    {
-      type: [String]
-    },
-    ingredients:
-    {
+    favorites: {
       type: [Schema.Types.ObjectId],
-      required: true
+    },
+    likes: {
+      type: Array,
+    },
+    youtube: {
+      type: String,
+    },
+    tags: {
+      type: [String],
+    },
+    ingredients: {
+      type: [Schema.Types.ObjectId],
+      required: true,
+      ref: "ingredients",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
     },
   },
   { versionKey: false, timestamps: true }
 );
+
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  category: Joi.string().required(),
+  time: Joi.string().required(),
+  ingredients: Joi.objectId().required(),
+  instructions: Joi.string().required(),
+});
+
+const schemas = {
+  addSchema,
+};
 
 recipeSchema.post("save", handleMongooseError);
 
@@ -101,5 +102,5 @@ const Recipe = model("recipe", recipeSchema);
 
 module.exports = {
   Recipe,
+  schemas,
 };
-
