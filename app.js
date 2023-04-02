0c7383244daf89cadc76b1be6c47bd221f93c43c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-02 14:41:12 +0300

    Update app.js
    add route "api/ingredients"

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 require("dotenv").config();
 
 const usersRouter = require("./routes/api/users");
-
 const recipesRouter = require("./routes/api/recipes");
+const ingredientsRouter = require("./routes/api/ingredients");
 
 const app = express();
 
@@ -18,14 +18,15 @@ app.use(express.static("public"));
 
 app.use("/api/users", usersRouter);
 app.use("/api/recipes", recipesRouter);
+app.use("/api/ingredients", ingredientsRouter);
 
 app.use((req, res) => {
-    res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-    const { status = 500, message = "Server error" } = err;
-    res.status(status).json({ message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = app;
