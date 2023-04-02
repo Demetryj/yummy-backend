d6961e2706e3d3c9b4a4ce33c85096e5bc8789a0
AntoBo <anton.bondarenkooooo@gmail.com>
2023-04-02 13:37:43 +0300

    406 resp on cloudinary

--- middlewares/uploadCloud.js
+++ middlewares/uploadCloud.js
@@ -1,6 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
+const { HttpError } = require("../helpers/");
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -8,12 +9,22 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_SECRET,
 });
 
+const alloweFormats = ["jpg", "png", "jpeg"];
+
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: "yummi-avatars",
+        file: (err, req, file, cb) => {
+            const splittedName = req.originalname.split(".");
+            const ext = splittedName[splittedName.length - 1];
+
+            if (!alloweFormats.includes(ext)) {
+                throw HttpError(406, "File format is not supported");
+            }
+        },
         public_id: (req, file) => req.user._id + "_" + file.originalname,
-        allowed_formats: ["jpg", "png", "jpeg"],
+        allowed_formats: alloweFormats,
         transformation: { width: 150, height: 150, crop: "fill" },
     },
 });
