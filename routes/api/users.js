ecf9e3518482880ddfef8d8ce57e0af545314a0e
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-02 14:44:11 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const { authValidators } = require("../../models/user");
-const { validateBodyWrapper, auth, uploadCloud } = require("../../middlewares/index");
+const {
+  validateBodyWrapper,
+  auth,
+  uploadCloud,
+} = require("../../middlewares/index");
 const { auth: ctrl } = require("../../controllers");
 
-router.post("/register", validateBodyWrapper(authValidators.register), ctrl.register);
+router.post(
+  "/register",
+  validateBodyWrapper(authValidators.register),
+  ctrl.register
+);
 
 router.post("/signin", validateBodyWrapper(authValidators.signin), ctrl.signin);
 
-//POST method for /update to ease front end life: form data works only with post by default
+// POST method for /update to ease front end life: form data works only with post by default
 router.post("/update", auth, uploadCloud.single("avatar"), ctrl.update);
 
 router.get("/logout", auth, ctrl.logout);
