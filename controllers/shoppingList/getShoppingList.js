7cb2b39cee268419f331e2469d1ad7f808991d8c
Ivan Dovhopol <ivandovhopol@gmail.com>
2023-04-03 22:30:59 +0300

    fix

--- controllers/shoppingList/getShoppingList.js
+++ controllers/shoppingList/getShoppingList.js
@@ -3,12 +3,10 @@ const { ShoppingList } = require('../../models');
 
 const getShoppingList = async (req, res) => {
   const { _id } = req.user;
-  const data = await ShoppingList.find({ owner: _id });
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
 
-  // const contacts = await Contact.find({ owner: _id }, '', { skip, limit: Number(limit) }).populate(
-  //   'owner',
-  //   '_id name email'
-  // );
+  const data = await ShoppingList.find({ owner: _id }, '', { skip, limit: Number(limit) });
 
   res.json(data);
 };
