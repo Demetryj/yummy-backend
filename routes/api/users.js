96b0bad713577c88f336f938b75fcca01c194ed1
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-04 21:46:04 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authValidators } = require('../../models/user');
+const { authValidators } = require("../../models/user");
 const {
   validateBodyWrapper,
   auth,
@@ -9,28 +9,27 @@ const {
 const { auth: ctrl } = require("../../controllers");
 const { ownRecipes: ownCtrl } = require("../../controllers");
 
-
 const { schemas } = require("../../models/recipe");
 
 router.post(
-  '/register',
+  "/register",
   validateBodyWrapper(authValidators.register),
   ctrl.register
 );
 
-router.post('/signin', validateBodyWrapper(authValidators.signin), ctrl.signin);
+router.post("/signin", validateBodyWrapper(authValidators.signin), ctrl.signin);
 
 // POST method for /update to ease front end life: form data works only with post by default
 
 router.post("/update", auth, uploadCloud.single("avatar"), ctrl.update);
 
-router.get('/logout', auth, ctrl.logout);
+router.get("/logout", auth, ctrl.logout);
 
-router.get('/current', auth, ctrl.current);
+router.get("/current", auth, ctrl.current);
 
-router.get('/current/subscribe/:subscribedToken', ctrl.updateSubscription);
+router.get("/current/subscribe/:subscribedToken", ctrl.updateSubscription);
 router.post(
-  '/current/subscribe',
+  "/current/subscribe",
   auth,
   validateBodyWrapper(authValidators.subscribe),
   ctrl.sendSubscriptionEmail
@@ -50,6 +49,5 @@ router.post(
 router.delete("/:userId/own-recipes/:recipeId", ownCtrl.removeOwnRecipes);
 
 router.get("/info/:userId", auth, ctrl.getUserInfo);
-
 
 module.exports = router;
