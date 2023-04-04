0d0143173ca86714219ba1ae64603b41d3ad8340
Volodymyr Kurov <vokur13@gmail.com>
2023-04-04 11:46:22 +0300

    Update recipes.js

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -1,23 +1,23 @@
-const express = require("express");
+const express = require('express');
 
-const { isValidId, auth } = require("../../middlewares");
+const { isValidId, auth } = require('../../middlewares');
 
-const { recipes: ctrl } = require("../../controllers");
+const { recipes: ctrl } = require('../../controllers');
 
 const router = express.Router();
 
-router.get("/main-page", auth, ctrl.getRecipes);
+router.get('/main-page', auth, ctrl.getRecipes);
 
-router.get("/:recipeId", auth, isValidId, ctrl.getRecipeById);
+router.get('/:recipeId', auth, isValidId, ctrl.getRecipeById);
 
-router.get("/category/list", auth, ctrl.getCategoriesList);
+router.get('/category/list', auth, ctrl.getCategoriesList);
 
-router.get("/category/:alias", auth, ctrl.getRecipesByCategory);
+router.get('/category/:alias', auth, ctrl.getRecipesByCategory);
 
-router.get("/all/popular", auth, ctrl.getRecipesPopular);
+router.get('/all/popular', auth, ctrl.getRecipesPopular);
 
 router.patch('/:recipeId/favorites', auth, isValidId, ctrl.updateFavorites);
 
-router.get('/favorites/favorites', auth, ctrl.getFavorites);
+router.get('/favorites/list', auth, ctrl.getFavorites);
 
 module.exports = router;
