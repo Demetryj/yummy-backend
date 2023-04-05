39200d4d47198dbfecb77ba3a22d867c2c2e3e7a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-05 15:13:26 +0300

    Update addOwnRecipes.js
    add preview

--- controllers/ownRecipes/addOwnRecipes.js
+++ controllers/ownRecipes/addOwnRecipes.js
@@ -1,17 +1,22 @@
 const { Recipe } = require("../../models");
-const { HttpError, ctrlWrapper } = require("../../helpers");
+const { ctrlWrapper } = require("../../helpers");
 
 const addOwnRecipes = async (req, res) => {
   const { _id } = req.user;
 
-  const { title, description, category, time, ingredients, instructions } =
-    req.body;
+  const {
+    title,
+    preview,
+    description,
+    category,
+    time,
+    ingredients,
+    instructions,
+  } = req.body;
 
-  if (!title) {
-    throw HttpError(409, "This title already exist");
-  }
   const result = await Recipe.create({
     title,
+    preview,
     description,
     category,
     time,
