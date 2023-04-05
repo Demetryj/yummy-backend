7ac27275a80fdfeffa0fd30b579de8727068c412
Volodymyr Kurov <vokur13@gmail.com>
2023-04-05 16:46:12 +0300

    favorites routes fixed

--- models/recipe.js
+++ models/recipe.js
@@ -1,10 +1,10 @@
-const { Schema, model } = require("mongoose");
-const Joi = require("joi");
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
 
-const { handleMongooseError } = require("../helpers");
+const { handleMongooseError } = require('../helpers');
 const {
   getCategoriesList,
-} = require("../controllers/recipes/getCategoriesList");
+} = require('../controllers/recipes/getCategoriesList');
 
 const recipeSchema = new Schema(
   {
@@ -47,11 +47,9 @@ const recipeSchema = new Schema(
     },
     favorites: [
       {
-
         type: Schema.Types.ObjectId,
-        ref: "user",
+        ref: 'user',
         required: false,
-
       },
     ],
     likes: {
@@ -69,11 +67,11 @@ const recipeSchema = new Schema(
     ingredients: {
       type: [Object],
       required: true,
-      ref: "ingredient",
+      ref: 'ingredient',
     },
     owner: {
       type: Schema.Types.ObjectId,
-      ref: "user",
+      ref: 'user',
     },
   },
   { versionKey: false, timestamps: true }
@@ -93,9 +91,9 @@ const schemas = {
   addSchema,
 };
 
-recipeSchema.post("save", handleMongooseError);
+recipeSchema.post('save', handleMongooseError);
 
-const Recipe = model("recipe", recipeSchema);
+const Recipe = model('recipe', recipeSchema);
 
 module.exports = {
   Recipe,
--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -1,28 +1,36 @@
-const express = require("express");
+const express = require('express');
 
-const { isValidId, auth } = require("../../middlewares");
+const { isValidId, auth } = require('../../middlewares');
 
-const { recipesControllers } = require("../../controllers");
+const { recipesControllers } = require('../../controllers');
 
 const router = express.Router();
 
-router.get("/main-page", auth, recipesControllers.getRecipes);
+router.get('/main-page', auth, recipesControllers.getRecipes);
 
-router.get("/:recipeId", auth, isValidId, recipesControllers.getRecipeById);
+router.get('/:recipeId', auth, isValidId, recipesControllers.getRecipeById);
 
-router.get("/category/list", auth, recipesControllers.getCategoriesList);
+router.get('/category/list', auth, recipesControllers.getCategoriesList);
 
-router.get("/category/:alias", auth, recipesControllers.getRecipesByCategory);
+router.get('/category/:alias', auth, recipesControllers.getRecipesByCategory);
 
-router.get("/all/popular", auth, recipesControllers.getRecipesPopular);
+router.get('/all/popular', auth, recipesControllers.getRecipesPopular);
 
+// ендпоінт для додавання рецептів до обраних
 router.patch(
-  "/:recipeId/favorites",
+  '/:recipeId/favorites/true',
   auth,
   isValidId,
-  recipesControllers.updateFavorites
+  recipesControllers.addFavorites
 );
-
-router.get("/favorites/list", auth, recipesControllers.getFavorites);
+// ендпоінт для видалення рецептів авторизованого користувача доданих цим же до обраних
+router.patch(
+  '/:recipeId/favorites/false',
+  auth,
+  isValidId,
+  recipesControllers.removeFavorites
+);
+// ендпоінт для отримання рецептів авторизованого користувача доданих ним же в обрані
+router.get('/favorites/list', auth, recipesControllers.getFavorites);
 
 module.exports = router;
