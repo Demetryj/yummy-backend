330411590b24e4780a0d3bad209175816bc1e7fa
MaryKotliar <glinskayam@ukr.net>
2023-04-06 13:17:59 +0300

    update getRecipeById

--- controllers/recipes/getRecipeById.js
+++ controllers/recipes/getRecipeById.js
@@ -1,13 +1,54 @@
-const { Recipe } = require('../../models/recipe');
-
-const { HttpError, ctrlWrapper } = require('../../helpers');
-
+const { Recipe } = require("../../models/recipe");
+const mongoose = require("mongoose");
+const { HttpError, ctrlWrapper } = require("../../helpers");
+const ObjectId = mongoose.Types.ObjectId;
 const getRecipeById = async (req, res) => {
   const { recipeId } = req.params;
-  const result = await Recipe.findById(recipeId);
+
+  const result = await Recipe.aggregate([
+    {
+      $match: {
+        _id: ObjectId(recipeId),
+      },
+    },
+    {
+      $lookup: {
+        from: "ingredients",
+        localField: "ingredients.id",
+        foreignField: "_id",
+        as: "ingr_nfo",
+      },
+    },
+    {
+      $set: {
+        ingredients: {
+          $map: {
+            input: "$ingredients",
+            in: {
+              $mergeObjects: [
+                "$$this",
+                {
+                  $arrayElemAt: [
+                    "$ingr_nfo",
+                    {
+                      $indexOfArray: ["$ingr_nfo._id", "$$this.id"],
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+    {
+      $unset: ["ingr_nfo", "ingredients.id"],
+    },
+  ]);
   if (!result) {
-    throw HttpError(404, 'Not found');
+    throw HttpError(404, "Not found");
   }
+
   res.json(result);
 };
 
