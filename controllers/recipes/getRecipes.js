5d323211df721c9b785877fae47abd3be836e882
AllaYukhym <yukhymenko.alla@gmail.com>
2023-04-08 11:25:00 +0200

    change name of varieble in getRecipies

--- controllers/recipes/getRecipes.js
+++ controllers/recipes/getRecipes.js
@@ -20,13 +20,13 @@ const getRecipes = async (req, res) => {
     }, {});
   };
 
-  const grouped = groupBy(recipes, "category"); //the variable "grouped" is object
+  const result = groupBy(recipes, "category"); //the variable "grouped" is object
 
-  Object.keys(grouped).forEach((key) => {
-    grouped[key] = grouped[key].slice(0, qtty);
+  Object.keys(result).forEach((key) => {
+    result[key] = result[key].slice(0, qtty);
   });
 
-  res.status(200).json({ grouped });
+  res.status(200).json({ result });
 };
 
 module.exports = { getRecipes };
