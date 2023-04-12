1e775125ad043e6735770e15d7298004ad1fcf7a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-12 16:22:15 +0300

    Update addToShoppingList.js

--- controllers/shoppingList/addToShoppingList.js
+++ controllers/shoppingList/addToShoppingList.js
@@ -1,17 +1,17 @@
-const { HttpError } = require('../../helpers');
-const { User, Ingredient } = require('../../models');
+const { HttpError } = require("../../helpers");
+const { User, Ingredient } = require("../../models");
 
 const addToShoppingList = async (req, res) => {
   const user = req.user;
 
-  //TODO: вписывать вручную данные
+  // TODO: вписывать вручную данные
   const { id: productId } = req.params;
   const { measure } = req.query;
 
   const ingredient = await Ingredient.findById(productId);
 
   if (!ingredient) {
-    throw HttpError(404, 'No such ingredient');
+    throw HttpError(404, "No such ingredient");
   }
   const result = await User.findByIdAndUpdate(
     user._id,
@@ -20,15 +20,15 @@ const addToShoppingList = async (req, res) => {
     },
     { new: true }
   )
-    .select('shoppingList')
+    .select("shoppingList")
     .populate({
-      path: 'shoppingList.productId',
-      ref: 'ingredients',
+      path: "shoppingList.productId",
+      ref: "ingredients",
     });
 
   const { shoppingList } = result.toObject();
 
-  shoppingList.forEach(ingr => {
+  shoppingList.forEach((ingr) => {
     ingr.title = ingr.productId.ttl;
     ingr.thumb = ingr.productId.thb;
     ingr.productId = ingr.productId._id;
