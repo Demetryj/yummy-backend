84512dbd8f9b0efbca4c5f34a22fdd3718424bfa
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-04-12 23:00:23 +0300

    Update getFavorites.js

--- controllers/recipes/getFavorites.js
+++ controllers/recipes/getFavorites.js
@@ -1,4 +1,6 @@
 const { Recipe } = require("../../models/recipe");
+const { HttpError } = require("../../helpers");
+const { setPaginationSlice } = require("../../helpers");
 
 const getFavorites = async (req, res) => {
   const { _id: owner } = req.user;
@@ -7,7 +9,24 @@ const getFavorites = async (req, res) => {
     { favorites: { $in: [owner] } },
     "title category area popularity"
   );
-  res.json(result);
+
+  const { page = 1, per_page = result.length } = req.query;
+
+  if (result.length === 0) {
+    return res.json({ totalHits: 0, meals: [] });
+  }
+
+  const pagination = setPaginationSlice(page, per_page, result.length);
+  if (!pagination) {
+    throw HttpError(400, "Incorrect pagination params");
+  }
+
+  res.json({
+    result: {
+      totalHits: result.length,
+      meals: result.slice(pagination.start, pagination.end),
+    },
+  });
 };
 
 module.exports = getFavorites;
