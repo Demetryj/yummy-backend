f7a6e78ff2053d84eb9e53fc627dd71e77b884bd
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-12 16:22:06 +0300

    Update removeFromShoppingList.js

--- controllers/shoppingList/removeFromShoppingList.js
+++ controllers/shoppingList/removeFromShoppingList.js
@@ -1,17 +1,19 @@
-const { HttpError } = require('../../helpers');
-const { User } = require('../../models');
+const { HttpError } = require("../../helpers");
+const { User } = require("../../models");
 
 const removeFromShoppingList = async (req, res) => {
   const user = req.user;
   const { id: productId } = req.params;
   const { measure } = req.query;
 
-  const productIndex = user.shoppingList.findIndex(item => {
-    return String(item.productId) === String(productId) && item.measure === measure;
+  const productIndex = user.shoppingList.findIndex((item) => {
+    return (
+      String(item.productId) === String(productId) && item.measure === measure
+    );
   });
 
   if (productIndex === -1) {
-    throw HttpError(404, `No product ${productId} in shopping list`);
+    throw HttpError(404, `No product with id=${productId} in shopping list`);
   }
 
   const deletedId = user.shoppingList.splice(productIndex, 1);
@@ -23,14 +25,14 @@ const removeFromShoppingList = async (req, res) => {
     },
     { new: true }
   )
-    .select('shoppingList')
+    .select("shoppingList")
     .populate({
-      path: 'shoppingList.productId',
-      ref: 'ingredients',
+      path: "shoppingList.productId",
+      ref: "ingredients",
     });
   const { shoppingList } = result.toObject();
 
-  shoppingList.forEach(ingr => {
+  shoppingList.forEach((ingr) => {
     ingr.title = ingr.productId.ttl;
     ingr.thumb = ingr.productId.thb;
     ingr.productId = ingr.productId._id;
