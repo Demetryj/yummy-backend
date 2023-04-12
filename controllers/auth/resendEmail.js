9463f3933fec23b9266151402e508f9847d2e576
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-12 16:23:55 +0300

    Update resendEmail.js

--- controllers/auth/resendEmail.js
+++ controllers/auth/resendEmail.js
@@ -1,26 +1,26 @@
-const confirmationEmail = require('../../data/confirmationEmail');
-const { HttpError, sendEmail } = require('../../helpers');
-const { User } = require('../../models');
+const confirmationEmail = require("../../data/confirmationEmail");
+const { HttpError, sendEmail } = require("../../helpers");
+const { User } = require("../../models");
 
 const { BASE_URL } = process.env;
 
 const resendEmail = async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
-  if (!user) throw HttpError(401, 'User with such email was not found');
-  if (user.verify) throw HttpError(400, 'Verification has already been passed');
+  if (!user) throw HttpError(404, "User with such email was not found");
+  if (user.verify) throw HttpError(409, "Verification has already been passed");
 
   const verifyLink = `${BASE_URL}/api/users/verify/${user.verificationToken}`;
   const resentEmail = {
     to: email,
-    subject: 'Re-send Verification Email Confirmation',
+    subject: "Re-send Verification Email Confirmation",
     html: confirmationEmail(verifyLink),
   };
 
   sendEmail(resentEmail);
 
   res.json({
-    message: 'You successfully verified your email!',
+    message: "You successfully verified your email!",
   });
 };
 
