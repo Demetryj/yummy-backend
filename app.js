bac7e23d04a64513ad320e683f63a46cdfd4f734
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-12 16:23:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
-const usersRouter = require('./routes/api/users');
-const recipesRouter = require('./routes/api/recipes');
-const searchRouter = require('./routes/api/search');
-const ingredientsRouter = require('./routes/api/ingredients');
-const shoppingListRouter = require('./routes/api/shoppingList');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+const usersRouter = require("./routes/api/users");
+const recipesRouter = require("./routes/api/recipes");
+const searchRouter = require("./routes/api/search");
+const ingredientsRouter = require("./routes/api/ingredients");
+const shoppingListRouter = require("./routes/api/shoppingList");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/api/users', usersRouter);
-app.use('/api/recipes', recipesRouter);
-app.use('/api/ingredients', ingredientsRouter);
-app.use('/api/search', searchRouter);
-app.use('/api/shopping-list', shoppingListRouter);
+app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/users", usersRouter);
+app.use("/api/recipes", recipesRouter);
+app.use("/api/ingredients", ingredientsRouter);
+app.use("/api/search", searchRouter);
+app.use("/api/shopping-list", shoppingListRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
