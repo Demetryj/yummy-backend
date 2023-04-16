54f49df91d33d1de1ed703105b2d2bc9926e874d
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-04-16 21:35:34 +0300

    fix: update SendGrid

--- controllers/recipes/getRecipesByQueryParams.js
+++ controllers/recipes/getRecipesByQueryParams.js
@@ -1,17 +1,17 @@
-const { Recipe } = require("../../models/recipe");
-const { aggregateOpts } = require("../../constants");
+const { Recipe } = require('../../models/recipe');
+const { aggregateOpts } = require('../../constants');
 
 const getRecipesByQueryParams = async (req, res) => {
-  const { keyword = "", page = 1, limit = 8 } = req.query;
+  const { keyword = '', page = 1, limit = 8 } = req.query;
   const curPage = +page;
   const skip = (curPage - 1) * +limit;
 
   const result = await Recipe.aggregate(
     aggregateOpts.getOptionsAggArr1({
-      $match: { title: { $regex: "^" + keyword, $options: "i" } },
+      $match: { title: { $regex: `${keyword}`, $options: 'i' } },
     })
   ).facet({
-    metaData: [{ $count: "total" }, { $addFields: { curPage } }],
+    metaData: [{ $count: 'total' }, { $addFields: { curPage } }],
     recipeData: [{ $skip: +skip }, { $limit: +limit }],
   });
 
