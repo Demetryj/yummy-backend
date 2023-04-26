979d4a9c18fbd062db1ad51a573d8dcf9affe1bd
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-04-26 23:30:34 +0300

    add cloudinary to own recipe ctrl

--- controllers/ownRecipes/addOwnRecipes.js
+++ controllers/ownRecipes/addOwnRecipes.js
@@ -1,21 +1,15 @@
-const { Recipe } = require("../../models");
+const { Recipe } = require('../../models');
 
 const addOwnRecipes = async (req, res) => {
   const { _id } = req.user;
+  const preview = req.file?.path;
 
-  const {
-    title,
-    category,
-    instructions,
-    description,
-    preview,
-    time,
-    ingredients,
-  } = req.body;
+  const { title, category, instructions, description, time, ingredients } = req.body;
 
   const result = await Recipe.create({
     title,
     preview,
+    thumb: preview,
     description,
     category,
     time,
